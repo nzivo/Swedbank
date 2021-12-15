194895f5390cf1bc428ab68e35c236635724a5ef
nzivo <johnnnzivo@gmail.com>
2021-12-16 00:17:14 +0300

    Added Custom Theme

--- src/App.js
+++ src/App.js
@@ -1,13 +1,38 @@
 import React from 'react'
+import {Introduction} from './pages/Introduction/Introduction'
 import {Navbar, Footer} from './components';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import './App.css'
+
+const swedBankTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#f35b1c'
+        },
+        secondary: {
+            main: '#257886'
+        },
+        tertiary1: "#5b8ad6",
+        tertiary2: "#c5569a"
+    },
+    typography: {
+        fontFamily: 'Source Sans Pro',
+        fontWeightLight: 400,
+        fontWeightRegular: 500,
+        fontWeightMedium: 600,
+        fontWeightBold: 700,
+    }
+})
+
 const App = () => {
     return (
-        <div>
-            <Navbar />
-            <h1>hey</h1>
-            <Footer />
-        </div>
+        <ThemeProvider theme={swedBankTheme}>
+            <div>
+                <Navbar />
+                <Introduction/>
+                <Footer />
+            </div>
+        </ThemeProvider>
     )
 }
 
